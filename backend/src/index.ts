34fbd04b79f33ac1e28de69bbe061c5140bc5801
Uday Bhatnagar <udaybhatnagar502@gmail.com>
2025-08-07 11:41:16 +0530

    Backend creation

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,11 @@
-import 
+import express from "express";
+
+const app=express();
+
+app.post('/signup',(req,res)=>{
+    const {name,password,email}=req.body;
+})
+
+app.post('/login',(req,res)=>{
+    const {name,password,email}=req.body;
+})
